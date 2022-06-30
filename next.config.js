a5a151a6b9c316a93eb6f068f139ac98b4c63c1d
hadis soleymani <hadissoleymani.it@gmail.com>
2022-06-30 00:44:33 -0700

    delete next sass

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
+const path = require('path')
 
-const path = require("path");
-const withSass = require("@zeit/next-sass");
-module.exports = withSass({
-  /* bydefault config  option Read For More Optioshere https://github.com/vercel/next-plugins/tree/master/packages/next-sass*/ cssModules: true,
-});
 module.exports = {
-  /* Add Your Scss File Folder Path Here */ sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
   },
-};
+}
--- pages/navbar.js
+++ pages/navbar.js
@@ -1,9 +1,28 @@
-import React from 'react'
-import styles from '../styles/navbar.module.scss'
-
+import Image from "next/image";
+import styles from "../styles/navbar.module.scss";
+import { AiOutlineSearch, AiOutlineShoppingCart } from "react-icons/ai";
 
 export const Navbar = () => {
   return (
-    <div className={styles.container}>navbar</div>
-  )
-}
+    <header className={styles.container}>
+      <div>
+        <div>
+          <Image src="/logo.svg" alt="logo" width={100} height={100} />
+        </div>
+        <nav className={styles.nav_right}>
+          <a href="#"> خانه</a>
+          <a href="#"> فروشگاه </a>
+          <a href="#"> درباره ی ما </a>
+          <a href="#"> تماس با ما </a>
+        </nav>
+        <nav className={styles.nav_left}>
+          <AiOutlineSearch />
+          <a href="#"> حساب کاربری</a>
+        </nav>
+        <div>
+          <AiOutlineShoppingCart />
+        </div>
+      </div>
+    </header>
+  );
+};
