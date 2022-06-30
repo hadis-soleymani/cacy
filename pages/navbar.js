6bc0228148527bcf52740b622706aeeb8a795fba
hadis soleymani <hadissoleymani.it@gmail.com>
2022-06-29 23:54:24 -0700

    add sass to project

---
+++ pages/navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from '../styles/navbar.module.scss'
+
+
+export const Navbar = () => {
+  return (
+    <div className={styles.container}>navbar</div>
+  )
+}
