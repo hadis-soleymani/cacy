a71ec0eec81d0a79654de15810ee7fed7acea4d2
hadis soleymani <hadissoleymani.it@gmail.com>
2022-07-07 07:02:04 -0700

    add comment

--- pages/navbar.js
+++ pages/navbar.js
@@ -1,20 +1,30 @@
-import React, { useState } from "react";
+// note: this file is header component
+
+import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+
+//style
 import styles from "../styles/navbar.module.scss";
+
+//icons
 import { AiOutlineSearch } from "react-icons/ai";
 import { FiShoppingBag } from "react-icons/fi";
+
+//components
 import HamburgerMenu from "./hamburgerMenu";
 
 const Navbar = () => {
   return (
     <header className={styles.header}>
+      {/* card container */}
       <div className={styles.card_container}>
         <div>
           <FiShoppingBag size={26} color="#fff" />
         </div>
       </div>
 
+      {/* search and profile container */}
       <nav className={styles.nav_left}>
         <div className={styles.icon_inside}>
           <input type="text" className={styles.input} />
@@ -26,19 +36,23 @@ const Navbar = () => {
         </select>
       </nav>
 
+      {/* right navbar container */}
       <nav className={styles.nav_right}>
-        <Link href="#" > خانه</Link>
+        <Link href="#"> خانه</Link>
         <Link href="#"> فروشگاه </Link>
         <Link href="#"> درباره ی ما </Link>
         <Link href="#"> تماس با ما </Link>
       </nav>
 
+      {/* logo container */}
       <div className={styles.logo_container}>
+        {/* show logo in desktop & tablet */}
         <div className={styles.logo}>
           <Image src="/logo.svg" alt="logo" width={100} height={100} />
         </div>
 
-        <HamburgerMenu></HamburgerMenu>
+        {/* hamburgger menu just in mobile size */}
+        <HamburgerMenu />
       </div>
     </header>
   );
