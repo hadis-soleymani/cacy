d342fa30626a944b03b64bb25f5004fa8caa5c63
hadis soleymani <hadissoleymani.it@gmail.com>
2022-07-09 09:02:06 -0700

    add alert

--- pages/authentication/sign_in.js
+++ pages/authentication/sign_in.js
@@ -1,5 +1,5 @@
-import React from "react";
-import Link from 'next/link'
+import React, { useState } from "react";
+import Link from "next/link";
 
 //styles
 import styles from "../../styles/auth.module.scss";
@@ -13,26 +13,45 @@ import { useFormik } from "formik";
 
 //function
 import validate from "./validation";
+
+//components
 import My_popup from "../my_popup";
 
 const Sign_in = () => {
+  //popup controller state
+  const [open, setOpen] = useState(false);
+
+  //formik state
   const formik = useFormik({
     initialValues: {
       email: "",
     },
     //call validation function
     validate,
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-    },
   });
+
+  //form submit button
+  const sign_in = (event) => {
+    event.preventDefault();
+
+    setOpen(!open);
+  };
 
   return (
     <Auth_Layout>
-      <My_popup/>
-      <form onSubmit={formik.handleSubmit} className={styles.form_container}>
+      {/* popup */}
+      <My_popup
+        title="عملیات موفقیت آمیز بود"
+        description="ایمیل تایید برای شما ارسال شد. لطفا وریفای نمایید"
+        open={open}
+        status="success"
+      />
+
+      {/* sign in form */}
+      <form className={styles.form_container}>
         <h1 className={styles.title}>فروشگاه کاکی</h1>
 
+        {/* email input */}
         <div className={styles.icon_inside}>
           <input
             className={styles.input}
@@ -41,20 +60,21 @@ const Sign_in = () => {
             name="email"
             type="email"
             onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
             value={formik.values.email}
           />
           <HiOutlineMailOpen size={20} />
         </div>
-
+        {/* show email error */}
         {formik.touched.email && formik.errors.email ? (
           <div className={styles.error}>{formik.errors.email}</div>
         ) : null}
 
-        <button className={styles.button} type="submit">
+        <button className={styles.button} onClick={sign_in}>
           ورود
         </button>
-        <p><Link href="/authentication/sign_up">ثبت نام</Link></p>
+        <p>
+          <Link href="/authentication/sign_up">ثبت نام</Link>
+        </p>
       </form>
     </Auth_Layout>
   );
--- pages/my_popup.js
+++ pages/my_popup.js
@@ -1,12 +1,70 @@
-import React from "react";
-import styles from '../styles/my_popup.module.scss'
-import Popup from "reactjs-popup";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+
+//styles
+import styles from "../styles/my_popup.module.scss";
 import "reactjs-popup/dist/index.css";
 
-const My_popup = () => (
-  <Popup trigger={<button> Trigger</button>} position="right center">
-    <div>Popup content here !!</div>
-  </Popup>
-);
+//libraries
+import Popup from "reactjs-popup";
+
+//icons
+import { AiOutlineClose } from "react-icons/ai";
+
+//style of popup container
+const contentStyle = {
+  maxWidth: "600px",
+  width: "86%",
+  borderRadius: "1rem",
+};
+
+const My_popup = ({ title, description, status, open }) => {
+  //for resolve hydration error (state pre-render)
+  const [isBrowser, setIsBrowser] = useState(false);
+
+  //for resolve hydration error
+  useEffect(() => setIsBrowser(true), []);
+
+  return isBrowser ? (
+    <Popup open={open} position="center" contentStyle={contentStyle}>
+      {(close) => (
+        <div className={styles.popup_container}>
+          <AiOutlineClose
+            size={"1.5rem"}
+            className={styles.close_icon}
+            onClick={() => {
+              close();
+            }}
+          />
+          <Image
+            src={
+              status === "success"
+                ? "/tick.svg"
+                : status === "failed"
+                ? "/incorrect.svg"
+                : status === "warning"
+                ? "/alert.svg"
+                : "/tick.svg"
+            }
+            alt="plant"
+            layout="fixed"
+            width={50}
+            height={50}
+          />
+          <h2>{title}</h2>
+          <p>{description}</p>
+          <hr />
+          <button
+            onClick={() => {
+              close();
+            }}
+          >
+            باشه
+          </button>
+        </div>
+      )}
+    </Popup>
+  ) : null;
+};
 
 export default My_popup;
