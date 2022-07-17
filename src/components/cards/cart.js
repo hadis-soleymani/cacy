4a5c2dd9fee0868c9e1292cc5c293a9679e88933
hadis soleymani <hadissoleymani.it@gmail.com>
2022-07-17 06:14:01 -0700

    add shoping card

---
+++ src/components/cards/cart.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+import styles from '../../styles/cart.module.scss'
+
+const Cart = () => {
+ 
+
+  return (
+    <div className={styles.container}>
+     
+    </div>
+  );
+};
+
+export default Cart;
--- src/pages/screens/shoping_card.js
+++ src/pages/screens/shoping_card.js
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useContext } from "react";
+
+import Cart from "../../components/cards/cart";
+
+//Component
+
+import styles from "../../styles/shoping_card.module.scss";
 
 const Shoping_card = () => {
+ 
+  
   return (
-    <div>Shoping_card</div>
-  )
-}
+    <div className={styles.container}>
+      <div>
+<Cart/>
+      </div>
+      
+        <div className={styles.purchase_details}>
+          <p>
+            <span>Total Items: </span>
+            
+          </p>
+          <p>
+            <span>Total Payments: </span>
+            
+          </p>
+          <div>
+            <button
+              className={styles.check_out}
+             
+            >
+              Check out
+            </button>
+            <br />
+            <button
+              className={styles.clear}
+              
+            >
+              clear
+            </button>
+          </div>
+        </div>
+      
+      {/* {state.checkout && (
+        <div className={styles.clear_checkout_div}>
+          <h3>Check Out Successfully</h3>
+          <Link to="/products">Buy More</Link>
+        </div>
+      )} */}
 
-export default Shoping_card
+      {/* {!state.checkout && state.itemsCounter === 0 && (
+        <div className={styles.clear_checkout_div}>
+          <h3>Want To Buy?</h3>
+          <Link to="/products">Go To Shop</Link>
+        </div>
+      )} */}
+    </div>
+  );
+};
+
+export default Shoping_card;
