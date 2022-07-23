91bb1fd4c759a6fed0974ce8aa1abc28034139de
hadis soleymani <hadissoleymani.it@gmail.com>
2022-07-23 11:12:54 -0700

    add formik

--- src/pages/authentication/sign_in.js
+++ src/pages/authentication/sign_in.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Link from "next/link";
-
+import { useFormik } from 'formik';
 //styles
 import styles from "../../styles/auth.module.scss";
 import Auth_Layout from "./auth_layout";
@@ -15,7 +15,21 @@ import Button from "../../components/shared/button";
 const Sign_in = () => {
   //popup controller state
   const [open, setOpen] = useState(false);
+  const formik = useFormik({
 
+    initialValues: {
+
+      email: '',
+
+    },
+
+    onSubmit: values => {
+
+      alert(JSON.stringify(values, null, 2));
+
+    },
+
+  });
   //form submit button
   const sign_in = (event) => {
     event.preventDefault();
@@ -34,22 +48,27 @@ const Sign_in = () => {
       />
 
       {/* sign in form */}
-      <form className={styles.form_container}>
+      <form className={styles.form_container} onSubmit={formik.handleSubmit}>
         <h1 className={styles.title}>فروشگاه کاکی</h1>
 
         {/* email input */}
         <div className={styles.icon_inside}>
           <input
             className={styles.input}
-            placeholder="email"
             id="email"
+
             name="email"
+   
             type="email"
+   
+            onChange={formik.handleChange}
+   
+            value={formik.values.email}
           />
           <HiOutlineMailOpen size={20} />
         </div>
 
-        <Button onClick={sign_in} width="100%" height="fit-content">
+        <Button type="submit" width="100%" height="fit-content">
           ورود
         </Button>
         <p>
