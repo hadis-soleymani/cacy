d5554681d1ae6d19c021bc29dc2c65d048c3ef80
hadis soleymani <hadissoleymani.it@gmail.com>
2022-07-23 12:09:43 -0700

    add validation

--- src/pages/authentication/sign_up.js
+++ src/pages/authentication/sign_up.js
@@ -1,6 +1,6 @@
-import React from "react";
-import Link from 'next/link'
-
+import React, { useState } from "react";
+import Link from "next/link";
+import { useFormik } from "formik";
 //styles
 import styles from "../../styles/auth.module.scss";
 import Auth_Layout from "./auth_layout";
@@ -8,17 +8,66 @@ import Auth_Layout from "./auth_layout";
 //icons
 import { HiOutlineMailOpen } from "react-icons/hi";
 import Button from "../../components/shared/button";
-
-
+import validate from "../../components/helper/validation";
+import My_popup from "../../components/shared/my_popup";
 
 const Sign_up = () => {
- 
-
+  //popup controller state
+  const [open, setOpen] = useState(false);
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+    },
+    validate,
+    onSubmit: (values) => {
+      //  alert(JSON.stringify(values, null, 2));
+      setOpen(!open);
+    },
+  });
   return (
     <Auth_Layout>
-      <form  className={styles.form_container}>
+      <My_popup
+        title="عملیات موفقیت آمیز بود"
+        description="ایمیل تایید برای شما ارسال شد. لطفا وریفای نمایید"
+        open={open}
+        status="success"
+      />
+
+      <form className={styles.form_container} onSubmit={formik.handleSubmit}>
         <h1 className={styles.title}>فروشگاه کاکی</h1>
 
+        <div className={styles.icon_inside}>
+          <input
+            className={styles.input}
+            placeholder="email"
+            id="firstName"
+            name="firstName"
+            type="firstName"
+            onChange={formik.handleChange}
+            value={formik.values.firstName}
+          />
+          <HiOutlineMailOpen size={20} />
+        </div>
+        {formik.errors.firstName ? (
+          <div className={styles.error}>{formik.errors.firstName}</div>
+        ) : null}
+        <div className={styles.icon_inside}>
+          <input
+            className={styles.input}
+            placeholder="email"
+            id="lastName"
+            name="lastName"
+            type="lastName"
+            onChange={formik.handleChange}
+            value={formik.values.lastName}
+          />
+          <HiOutlineMailOpen size={20} />
+        </div>
+        {formik.errors.lastName ? (
+          <div className={styles.error}>{formik.errors.lastName}</div>
+        ) : null}
         <div className={styles.icon_inside}>
           <input
             className={styles.input}
@@ -26,43 +75,20 @@ const Sign_up = () => {
             id="email"
             name="email"
             type="email"
-            
+            onChange={formik.handleChange}
+            value={formik.values.email}
           />
           <HiOutlineMailOpen size={20} />
         </div>
-
-       
-          <div className={styles.icon_inside}>
-          <input
-            className={styles.input}
-            placeholder="email"
-            id="email"
-            name="email"
-            type="email"
-           
-          />
-          <HiOutlineMailOpen size={20} />
-        </div>
-
-      
-         <div className={styles.icon_inside}>
-          <input
-            className={styles.input}
-            placeholder="email"
-            id="email"
-            name="email"
-            type="email"
-           
-          />
-          <HiOutlineMailOpen size={20} />
-        </div>
-
-      
-
-        <Button  type="submit" width="fit-content" height="fit-content">
+        {formik.errors.email ? (
+          <div className={styles.error}>{formik.errors.email}</div>
+        ) : null}
+        <Button type="submit" width="100%" height="fit-content">
           ثبت نام
         </Button>
-        <p><Link href="/authentication/sign_in"> ورود</Link></p>
+        <p>
+          <Link href="/authentication/sign_in"> ورود</Link>
+        </p>
       </form>
     </Auth_Layout>
   );
