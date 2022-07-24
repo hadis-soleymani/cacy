38bbb6f0451fd511ed6e732f96897ed89f2eecda
hadis soleymani <hadissoleymani.it@gmail.com>
2022-07-24 03:06:44 -0700

    add card

--- src/components/cards/cart.js
+++ src/components/cards/cart.js
@@ -7,13 +7,78 @@ import Button_card from "./button_card";
 
 const Cart = () => {
   return (
-    <div>
-       <Button_card/>
-    
+    <div className={styles.container}>
+      <table>
+        <tbody>
+          <tr>
+            <th>محصول</th>
+            <th> </th>
+            <th>قیمت</th>
+            <th>تعداد</th>
+            <th>جمع کل</th>
+            <th> </th>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.img}>
+                <Image src="/cuctus2.png" alt="logo" width={40} height={60} />
+              </div>
+            </td>
+            <td>
+              <p>کاکتوس گلدان قرمز</p>
+              <p>قرمز</p>
+            </td>
+            <td>50000</td>
+            <td >
+              <Button_card />
+            </td>
+            <td> 100,000</td>
+            <td>
+              <AiOutlineClose size={28} className={styles.icon} />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.img}>
+                <Image src="/cuctus2.png" alt="logo" width={40} height={60} />
+              </div>
+            </td>
+            <td>
+              <p>کاکتوس گلدان قرمز</p>
+              <p>قرمز</p>
+            </td>
+            <td>50000</td>
+            <td>
+              <Button_card />
+            </td>
+            <td> 100,000</td>
+            <td>
+              <AiOutlineClose size={28} className={styles.icon} />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.img}>
+                <Image src="/cuctus2.png" alt="logo" width={40} height={60} />
+              </div>
+            </td>
+            <td>
+              <p>کاکتوس گلدان قرمز</p>
+              <p>قرمز</p>
+            </td>
+            <td>50000</td>
+            <td>
+              <Button_card />
+            </td>
+            <td> 100,000</td>
+            <td>
+              <AiOutlineClose size={28} className={styles.icon} />
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
 
 export default Cart;
-
-  
