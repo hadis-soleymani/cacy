a3951c48e2497820e804b5d3e0f809e0985115d9
hadis soleymani <hadissoleymani.it@gmail.com>
2022-08-15 12:57:02 -0700

    change navbar to header

--- src/pages/screens/shoping_card.tsx
+++ src/pages/screens/shoping_card.tsx
@@ -4,13 +4,14 @@ import Order_summery from "../../components/cards/order_summery";
 import Header from "../../components/shared/header";
 import Desktop_table from "../../components/cards/desktop_table";
 import Mobile_table from "../../components/cards/mobile_table";
+import Navbar from "../../components/shared/navbar";
 
 //Component
 
 const Shoping_card = () => {
   return (
     <>
-     <Header/>
+    <Navbar/>
     <div className={styles.container}>
      
       <div className={styles.desktop}><Desktop_table/></div>
