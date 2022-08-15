41fb6f8125f4f4a970f65939e3b71603dfd78f2d
hadis soleymani <hadissoleymani.it@gmail.com>
2022-08-15 12:48:47 -0700

    select option async with search value

--- src/components/shared/dropdown/dropdown.tsx
+++ src/components/shared/dropdown/dropdown.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Select, { StylesConfig } from "react-select";
-//import AsyncSelect from 'react-select/async'
+import AsyncSelect from "react-select/async";
 
 interface Props {
   options: { value: string; label: string }[];
   value?: string;
-  onChange?:  (name:string,value:string) => void;
-  name?:string;
+  onChange?: (name: string, value: string) => void;
+  name?: string;
 }
 //const options = [{ value: "Hormuzgan", label: "هرمزگان" }];
 const colourStyles: StylesConfig = {
@@ -34,19 +34,29 @@ const colourStyles: StylesConfig = {
     };
   },
 };
-const Dropdown: React.FC<Props> = ({ options, value,onChange,name }) => {
+
+const Dropdown: React.FC<Props> = ({ options, onChange, name }) => {
   const handleChange = (value: string) => {
     // this is going to call setFieldValue and manually update values.topcis
     onChange(name, value);
   };
 
+  const LoadOptions = (searchValue: string, callback: any) => {
+    setTimeout(() => {
+      const filterOptions = options.filter((options) =>
+        options.label.toLowerCase().includes(searchValue.toLowerCase())
+      );
+      callback(filterOptions);
+    }, 2000);
+  };
+
   return (
     <div style={{ width: "100%" }}>
-      <Select
-        options={options}
+      <AsyncSelect
+        loadOptions={LoadOptions}
         placeholder="نام استان خود را وارد کنید"
         styles={colourStyles}
-        value={value}
+        defaultOptions //for show options without search
         onChange={handleChange}
       />
     </div>
