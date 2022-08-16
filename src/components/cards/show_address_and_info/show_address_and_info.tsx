13c1f0dd44f62f35dfa9fc45527416451e7355b9
hadis soleymani <hadissoleymani.it@gmail.com>
2022-08-16 10:36:34 -0700

    add components

---
+++ src/components/cards/show_address_and_info/show_address_and_info.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './show_address_and_info.module.scss'
+
+const Show_address_and_info = () => {
+  return (
+    <div>Show_address_and_info</div>
+  )
+}
+
+export default Show_address_and_info
