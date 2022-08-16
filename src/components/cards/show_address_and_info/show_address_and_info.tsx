4ca9638274a5b3ebfea3657262b5ff397e758bc6
hadis soleymani <hadissoleymani.it@gmail.com>
2022-08-16 12:50:24 -0700

    add payment

--- src/components/cards/show_address_and_info/show_address_and_info.tsx
+++ src/components/cards/show_address_and_info/show_address_and_info.tsx
@@ -5,35 +5,35 @@ import styles from "./show_address_and_info.module.scss";
 const Show_address_and_info = () => {
   return (
     <>
-    <div className={styles.container}>
-      <h3>آدرس اول</h3>
-      <ul>
-        <li>
-          <p>
-            نام :<span>حدیث</span>
-          </p>
-        </li>
-        <li>
-          <p>
-            نام خانوادگی :<span>سلیمانی</span>
-          </p>
-        </li>
-        <li>
-          <p>
-            شماره تماس :<span>09380457496</span>
-          </p>
-        </li>
-      </ul>
-      <ul>
-        <li>
-          <p>
-            آدرس:<span>هرمزگان ، بندر عباس ،خیابان هتل هما</span>
-          </p>
-        </li>
-        <Link href="#">ویرایش</Link>
-      </ul>
-    </div>
-    <h2 className={styles.add_adress}>افزودن آدرس جدید</h2>
+      <div className={styles.container}>
+        <h3>آدرس اول</h3>
+        <ul>
+          <li>
+            <p>
+              نام :<span>حدیث</span>
+            </p>
+          </li>
+          <li>
+            <p>
+              نام خانوادگی :<span>سلیمانی</span>
+            </p>
+          </li>
+          <li>
+            <p>
+              شماره تماس :<span>09380457496</span>
+            </p>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <p>
+              آدرس:<span>هرمزگان ، بندر عباس ،خیابان هتل هما</span>
+            </p>
+          </li>
+          <Link href="#">ویرایش</Link>
+        </ul>
+      </div>
+      <h2 className={styles.add_adress}>افزودن آدرس جدید</h2>
     </>
   );
 };
--- src/pages/screens/payment/payment.tsx
+++ src/pages/screens/payment/payment.tsx
@@ -1,8 +1,32 @@
 import React from "react";
+import Link from "next/link";
+import Order_summery from "../../../components/cards/order_summery";
 import Show_address_and_info from "../../../components/cards/show_address_and_info/show_address_and_info";
-
+import styles from "./payment.module.scss";
+import Button from "../../../components/shared/button";
 const Payment = () => {
-  return <Show_address_and_info />;
+  return (
+    <div className={styles.container}>
+      <Show_address_and_info />
+      <Order_summery />
+
+      <div className={styles.about_payment}>
+      <p>
+        پرداخت امن به وسیله ی کلیه ی کارت های عضو شتاب از طریق درگاه زرین پال
+      </p>
+      <div className={styles.rules_div}>
+        <p>
+          من<span> قوانین و مقررات </span>را خوانده ام و آن را می پذیرم
+        </p>
+        <input type="checkbox" />
+      </div>
+      
+      </div>  
+      <Button width="240px" height="fit-content">
+        <Link href="/screens/payment/payment">تسویه حساب</Link>
+      </Button>
+    </div>
+  );
 };
 
 export default Payment;
